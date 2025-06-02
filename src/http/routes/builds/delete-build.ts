acf0eb7ffde1d49754e0a4c1e39e146c2403cb5a
YuriVini <yuri.ybgt.yfo@gmail.com>
2025-06-02 16:09:47 -0300

    chore: add authentication for delete build

--- src/http/routes/builds/delete-build.ts
+++ src/http/routes/builds/delete-build.ts
@@ -2,53 +2,58 @@ import { z } from "zod";
 import { type FastifyInstance } from "fastify";
 import { type ZodTypeProvider } from "fastify-type-provider-zod";
 
+import { auth } from "@/http/middlewares/auth";
+
 import { prisma } from "../../../lib/prisma";
 import { NotFoundError } from "../_errors/not-found";
 import { UnauthorizedError } from "../_errors/unauthorized";
 
 export async function deleteBuild(app: FastifyInstance) {
-  app.withTypeProvider<ZodTypeProvider>().delete(
-    "/builds/:id",
-    {
-      schema: {
-        tags: ["builds"],
-        summary: "Delete a build",
-        params: z.object({
-          id: z.string().uuid(),
-        }),
-        response: {
-          500: z.null(),
-          204: z.null(),
-          403: z.object({
-            message: z.string(),
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .delete(
+      "/builds/:id",
+      {
+        schema: {
+          tags: ["builds"],
+          summary: "Delete a build",
+          params: z.object({
+            id: z.string().uuid(),
           }),
-          404: z.object({
-            message: z.string(),
-          }),
+          response: {
+            500: z.null(),
+            204: z.null(),
+            403: z.object({
+              message: z.string(),
+            }),
+            404: z.object({
+              message: z.string(),
+            }),
+          },
         },
       },
-    },
-    async (request, reply) => {
-      const { id } = request.params;
-      const userId = await request.getCurrentUserId();
+      async (request, reply) => {
+        const { id } = request.params;
+        const userId = await request.getCurrentUserId();
 
-      const build = await prisma.build.findUnique({
-        where: { id },
-      });
+        const build = await prisma.build.findUnique({
+          where: { id },
+        });
 
-      if (!build) {
-        throw new NotFoundError("Build não encontrada");
+        if (!build) {
+          throw new NotFoundError("Build não encontrada");
+        }
+
+        if (build.userId !== userId) {
+          throw new UnauthorizedError("Não autorizado");
+        }
+
+        await prisma.build.delete({
+          where: { id },
+        });
+
+        return reply.status(204).send();
       }
-
-      if (build.userId !== userId) {
-        throw new UnauthorizedError("Não autorizado");
-      }
-
-      await prisma.build.delete({
-        where: { id },
-      });
-
-      return reply.status(204).send();
-    }
-  );
+    );
 }
